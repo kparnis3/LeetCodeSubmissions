04923c22bca060bda78b6c10629ba4ae4544d192
Kian Parnis <kian.parnis.20@um.edu.mt>
2023-05-13 11:38:20 +0200

    Time: 0 ms (100.00%), Space: 42.9 MB (18.55%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        
+        int[] result = new int[nums.length];
+
+        //First case always equal
+        result[0] = nums[0];
+
+        for(int i = 1; i<nums.length; i++)
+        {
+            result[i] = nums[i] + result[i-1];
+        }
+
+        return result;
+    }
+}
