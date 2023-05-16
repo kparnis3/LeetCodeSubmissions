37cf96be22d48bf091ac6d5ad38af169137a2b1b
Kian Parnis <kian.parnis.20@um.edu.mt>
2023-05-16 12:57:15 +0200

    Time: 0 ms (100.00%), Space: 39.7 MB (22.38%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    private Map<Integer, Integer> cache = new HashMap<>();
+    
+    public int fib(int n) {
+        return DFS(n); // Taking top-bottom approach 
+    }
+
+     public int DFS(int i)
+     {   
+        //Base case
+        if  (i==0) return 0;
+        if  (i==1) return 1;
+        
+        //Case already explored
+        if(cache.containsKey(i)) return cache.get(i);
+        
+        int result = 0;
+         
+        //Open left and right
+        result = DFS(i-1) + DFS(i-2);
+        cache.put(i, result);
+        
+        return result;
+                     
+    }
+}
