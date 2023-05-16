761e9b48abc4fefef54b4184e9c2b1af11dae1f4
Kian Parnis <kian.parnis.20@um.edu.mt>
2023-05-16 12:48:31 +0200

    Time: 0 ms (100.00%), Space: 39.1 MB (93.03%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,39 @@
+class Solution {
+    
+    private Map<Integer, Integer> cache = new HashMap<>();
+    private int maxN;
+    
+    public int climbStairs(int n) {
+     /*               0 n = 2
+                     / \ 
+                    1   2
+                   /
+                  1+1
+                
+     */
+        maxN = n;
+        return DFS(0);
+    }
+    
+    
+    public int DFS(int i)
+    {   
+        //Base case
+        if  (i==maxN) return 1; // Total should be 1
+        if  (i>maxN) return 0; //Out of bounds 
+        
+        //Case already explored
+        if(cache.containsKey(i)) return cache.get(i);
+        
+        int result = 0;
+        //Open left and right
+        result = DFS(i+1) + DFS(i+2);
+        cache.put(i, result);
+        
+        
+        return result;
+        
+        
+                
+    }
+}
