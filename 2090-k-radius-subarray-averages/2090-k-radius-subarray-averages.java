d0fe8b1ebdb4c85efefd132cffbc19136fd44401
Kian Parnis <kian.parnis.20@um.edu.mt>
2023-06-20 08:55:10 +0200

    Time: 11 ms (71.66%), Space: 59.6 MB (85.01%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        int n = nums.length;
+        int [] result = new int[n];
+        Arrays.fill(result,-1); //start with result array all                              -1
+        int length = 2*k+1; //Left, Right and Middle
+        
+        if (length > n){
+            return result;
+        }
+        
+        long sum = 0;
+        
+        for(int i=0;i<length; i++){ //Sum starting at index zero 
+            sum += nums[i];
+        }
+        
+        result[k] = (int) (sum / length); //First case 
+        
+        int startPoint = 0;
+        for(int lastPoint = length; lastPoint < n; lastPoint++)         {
+            sum = sum - nums[startPoint] + nums[lastPoint];
+            startPoint++;
+            result[lastPoint-k] = (int) (sum / length); //Compute rest
+        }
+        
+        return result;
+    }
+}
